e78c8cf04bb4e890e9e3c44eab7d1bdb6c954c60
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 12:18:08 +0100

    Implemented trignometric functions

--- assignment.py
+++ assignment.py
@@ -12,5 +12,20 @@ if cmd == '1':
     print yval
     plt.plot(xval, yval)
     plt.show()
+elif cmd == '2':
+    yval = np.sin(xval)
+    #print yval
+    plt.plot(xval, yval)
+    plt.show()
+elif cmd == '3':
+    yval = np.cos(xval)
+    #print yval
+    plt.plot(xval, yval)
+    plt.show()
+if cmd == '4':
+    yval = np.tan(xval)
+    #print yval
+    plt.plot(xval, yval)
+    plt.show()
 else:
     print "yval will be populated by some other funtion"
