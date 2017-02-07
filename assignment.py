26c48541016b500626a9fc4015f764d0d2160bae
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 12:13:02 +0100

    polynomial funtions(x**2 and x**3) implemented

--- assignment.py
+++ assignment.py
@@ -9,7 +9,19 @@ xval = np.arange(-5.0, 5.0, 0.1)
 if cmd == '1':
     f = lambda x:x
     yval = f(xval)
-    print yval
+    #print yval
+    plt.plot(xval, yval)
+    plt.show()
+if cmd == '2':
+    f = lambda x:x**2
+    yval = f(xval)
+    #print yval
+    plt.plot(xval, yval)
+    plt.show()
+if cmd == '3':
+    f = lambda x:x**3
+    yval = f(xval)
+    ########print yval
     plt.plot(xval, yval)
     plt.show()
 else:
