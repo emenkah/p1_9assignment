576d69462a78f96165b8c706f0689b65dac65654
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 12:30:00 +0100

    Exp and Sqrt functions implemented and Usage mesg updated

--- assignment.py
+++ assignment.py
@@ -11,5 +11,19 @@ if cmd == '1':
     yval = f(xval)
     plt.plot(xval, yval)
     plt.show()
+if cmd == '5':
+    yval = np.exp(xval)
+    plt.plot(xval, yval)
+    plt.show()
+if cmd == '6':
+    yval = np.sqrt(abs(xval))
+    plt.plot(xval, yval)
+    plt.show()
 else:
     print "Usage: Enter '1' to use the lambda function x to populate yval"
+    print "Usage: Enter '5' to use the exp  function  to populate yval"
+    print "Usage: Enter '6' to use the sqrt(abs(x)) functions  to populate yval"
+    
+
+
+
