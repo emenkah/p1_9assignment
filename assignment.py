59ae399a6ea6ce7800af262a4b1364835ae5aa8f
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 12:25:17 +0100

    Usage message implemented

--- assignment.py
+++ assignment.py
@@ -9,8 +9,7 @@ xval = np.arange(-5.0, 5.0, 0.1)
 if cmd == '1':
     f = lambda x:x
     yval = f(xval)
-    print yval
     plt.plot(xval, yval)
     plt.show()
 else:
-    print "yval will be populated by some other funtion"
+    print "Usage: Enter '1' to use the lambda function x to populate yval"
